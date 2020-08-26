47be45c8445e4cb4b72f4c58e33147829a55ba6c
Isaac <isaackhoo@hotmail.sg>
2020-08-26 19:06:33 +0800

    added office tcp keys

--- src/WCS/TCP/TCPKeys.cpp
+++ src/WCS/TCP/TCPKeys.cpp
@@ -11,3 +11,9 @@ namespace TCP
     const String serverIp = "192.168.201.97";
     const int serverPort = 45555;
 }; // namespace TCP
+
+// namespace TCP
+// {
+//     const String serverIp = "192.168.201.45";
+//     const int serverPort = 55555;
+// }; // namespace TCP
